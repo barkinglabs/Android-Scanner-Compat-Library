94d5ce9389df7092a2b404bd3041a38d18c125fe
Roum Ines <roum@tryfi.com>
2021-06-02 18:36:08 -0400

    device address null check

--- scanner/src/main/java/no/nordicsemi/android/support/v18/scanner/BluetoothLeScannerImplLollipop.java
+++ scanner/src/main/java/no/nordicsemi/android/support/v18/scanner/BluetoothLeScannerImplLollipop.java
@@ -194,10 +194,12 @@ import androidx.annotation.RequiresPermission;
 	@NonNull
 	/* package */ android.bluetooth.le.ScanFilter toNativeScanFilter(@NonNull final ScanFilter filter) {
 		final android.bluetooth.le.ScanFilter.Builder builder = new android.bluetooth.le.ScanFilter.Builder();
-		builder.setDeviceAddress(filter.getDeviceAddress())
-				.setDeviceName(filter.getDeviceName())
+		builder.setDeviceName(filter.getDeviceName())
 				.setServiceUuid(filter.getServiceUuid(), filter.getServiceUuidMask())
 				.setManufacturerData(filter.getManufacturerId(), filter.getManufacturerData(), filter.getManufacturerDataMask());
+
+		if (filter.getDeviceAddress() != null)
+			builder.setDeviceAddress(filter.getDeviceAddress());
 
 		if (filter.getServiceDataUuid() != null)
 			builder.setServiceData(filter.getServiceDataUuid(), filter.getServiceData(), filter.getServiceDataMask());
